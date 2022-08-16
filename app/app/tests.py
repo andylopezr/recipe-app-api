f368622459f2cc44c8c28d582d91191cc14dddbb
Andy Lopez <profesor.de.ingles@aol.com>
2022-08-16 14:44:31 -0500

    |- FIXED -| lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,6 +4,7 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Tests the calc module"""
 
@@ -12,3 +13,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_sub_numbers(self):
+        """Tests subtracting numbers together"""
+        res = calc.sub(15, 10)
+
+        self.assertEqual(res, 5)
