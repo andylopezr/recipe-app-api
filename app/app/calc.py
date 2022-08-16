911e020ed2bdbd4d68e635a02a15bb322154e24d
Andy Lopez <profesor.de.ingles@aol.com>
2022-08-16 14:29:40 -0500

    |- ADDED -| test files

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Adds x and y and return result."""
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Tests the calc module"""
+
+    def test_add_numbers(self):
+        """Tests adding numbers together"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
