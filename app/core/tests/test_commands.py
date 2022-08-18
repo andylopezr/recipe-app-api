1c21bd1d8bec0aa721be67d12e602083682766e3
Andy Lopez <profesor.de.ingles@aol.com>
2022-08-18 12:00:31 -0500

    |- FIXED -| typo

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,10 +3,10 @@ Tests custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
-from django.db.utils import OperationError
+from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
